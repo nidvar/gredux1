2c73815ecb8b7e0ac369cf8868270b27bfb1ec3d
Nidvar <djayshine87@hotmail.com>
2020-09-18 14:20:40 +1000

    created pokemon list component in component folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Pokemon_list from './components/Pokemon_list'
 
 function App() {
   return (
     <div className="App">
-      <h1>Integrating redux</h1>
+      <Pokemon_list />
     </div>
   );
 }
---
+++ src/components/Pokemon_list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pokemon_list = ()=>{
+    return(
+        <div>
+            <h1>Pokemon List</h1>
+        </div>
+    )
+}
+
+export default Pokemon_list
